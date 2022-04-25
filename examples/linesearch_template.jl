42c5e979669e1672f14df401e699e8d7324e07ec
Jorge Vieyra <jorge.vieyra.salas@asml.com>
2022-04-25 18:00:38 +0200

    Added sample bracketing function according to brent-dekker. Still need to check that it works correctly and add unit tests

--- examples/linesearch_template.jl
+++ examples/linesearch_template.jl
@@ -12,7 +12,7 @@
     maxfev::Int = 100
 end
 
- 
+
  function (ls::SampleLineSearch)(df::AbstractObjective, x::AbstractArray{T},
                            s::AbstractArray{T}, α::Real,
                            x_new::AbstractArray{T}, phi_0::Real, dphi_0::Real) where T
@@ -20,20 +20,20 @@ end
      # or
      # ϕdϕ = make_ϕdϕ(df, x_new, x, s)
 
-     ls(ϕdϕ, alpha, ϕ_0, dϕ_0)
+     return ls(ϕdϕ, α, ϕ_0, dϕ_0)
     end
- 
+
  (ls::SampleLineSearch)(ϕ, dϕ, ϕdϕ, phi_0, dphi_0) = ls(ϕ, ϕdϕ, phi_0, dphi_0)
- 
+
  # TODO: Should we deprecate the interface that only uses the ϕ and ϕd\phi arguments?
  function (ls::SampleLineSearch)(ϕ, ϕdϕ,
                            phi_0::Real,
                            dphi_0::Real) where T # Should c and phi_0 be same type?
             @unpack f_tol, gtol, x_tol, alphamin, alphamax, maxfev = ls
- 
-    # Do stuff ... 
+
+    # Do stuff ...
 
     # Include optimizer for argmin_α f(x + α * sn)
 
-    return  α, sn # It must return the step size α and the search direction at step_n
+    return  α, ϕ(α) # It must return the step size α and evalauted function
  end
---
+++ src/nlconjugategradient.jl
@@ -0,0 +1,128 @@
+
+# Linesearch
+# LineSearch(this, reltol, abstol, initialbracketingrange)
+
+
+
+# 1. Calculate steepeste descent
+# 2. Set initial search direction to steepest descent -> g0
+# 3. Set d0 <- g0
+# 4. 
+
+const Φ = (1 + √5) / 2 # Golden Ratio
+const ϵ_min = 1e-21 # Tolerance to avoid division by zero
+
+# Need a better place for this
+const growLimit = 100
+const maxEvaluations = 500
+
+function bracket_minimum(f, x=0; s=1e-2, k=2.0)
+    a, ya = x, f(x)
+    b, yb = a+s, f(a+s)
+    if yb > ya
+        a, b = b, a
+        ya, yb = yb, ya
+        s = -s
+    end
+
+    while true
+        c, yc = b+s,  f(b+s)
+        if yc > yb
+            return a < c? (a,c) : (c, a)
+        end
+        a, ya, b, yb = b, yb, c, yc
+        s *= k
+    end
+end
+
+
+bisection(df, a, b, ϵ) = bisection(df, a, b, ϵ=ϵ)
+function bisection(df, a::T, b::T; ϵ=eps(T)) where T
+    if a > b; a,b = b, a; end # ensure a < b
+
+    ya, yb = df(a), df(b)
+    if ya == zero(T); b = a; end
+    if yb == zero(T); a = b; end
+ 
+    while b - a > ϵ
+        x = (a+b)/2
+        y = df(x)
+        if y == zero(T)
+            a,b = x, x
+        elseif sign(y) == sign(ya)
+            a = x
+        else
+            b = x
+        end
+    end
+    return (a,b)
+end
+
+
+function brentdekker(f, a::T, b::T) where T <: Real
+    fa, fb = f(a), f(b)
+
+    # assuming minimization
+    if fa < fb
+        b, a = a, b
+        fa, fb = fb, fa
+    end
+
+    c = b + Φ * (b - a)
+    fc = f(c)
+
+    while fc < fb
+        tmp1 = (b - a) * (fb - fc)
+        tmp2 = (b - c) * (fb - fa)
+
+        val = tmp2 - tmp1
+        denom = abs(val) < ϵ_min ? 2 * ϵ_min : 2 * val
+
+        w = b - ((b - c) * tmp2 - (b - a) * tmp1) / denom
+        wlim = b + growLimit * (c - b)
+
+        local fw
+        if (w - c) * (b - w) > zero(T)
+            fw = f(w)
+            if fw < fc
+                a, b = b, w
+                fa, fb = fb, fw
+                break # IS this needed?
+            elseif fw > fb
+                c, fc = w, fw
+                break # IS this needed?
+            end
+            w = c + Φ * (c - b)
+            fw = f(w)
+        elseif (w - wlim) * (wlim - c) >= zero(T)
+            w = wlim
+            fw = f(w)
+        elseif (w - wlim) * (c - w) > zero(T)
+            fw = f(w)
+            if fw < fc
+                b, c = c, w
+                w = c + Φ * (c - b)
+                fb, fc = fc, fw
+                fw = f(w)
+            end
+        else
+            w = c + Φ * (c - b)
+            fw = f(w)
+        end
+
+        a, fa = b, fb
+        b, fb = c, fc
+        c, fc = w, fw
+    end
+
+    lo, flo   = a, fa
+    mid, fmid = b, fb
+    hi, fhi   = c, fc
+
+    if lo > hi
+        lo, hi = hi, lo
+        flo, fhi = fhi, flo
+    end
+
+    return lo, hi, flo, fhi, mid, fmid
+end
